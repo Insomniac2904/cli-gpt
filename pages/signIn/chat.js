0676f0e83788287b16932f875d4871a0d9f7929a
Adarsh <adarshkumar7478@gmail.com>
2023-06-16 01:38:26 +0530

    added type animation to chatjs and fixed minor bugs

--- pages/signIn/chat.js
+++ pages/signIn/chat.js
@@ -1,52 +1,93 @@
-const { inquirer } = require("inquirer");
-
-const chat = async (currUser) => {
-  console.log("Hello there! How may I help?");
-  while (ask()) {
-    console.log(ask());
-  }
-  const SignInPage = require("./sign-in-page");
-  console.log("Goodbye!");
-  SignInPage();
-};
-
-const ask = () => {
-  inquirer
-    .prompt({
-      type: "input",
-      name: "question",
-      message: "Enter Text Here:",
-    })
-    .then(async (ans) => {
-      if (ans.question.toLowerCase.trim() == "exit") {
-        return 0;
-      } else {
-        return await apiReq(ans.question);
-      }
-    });
-};
+const { Configuration, OpenAIApi } = require("openai");
+const highlight = require("cli-highlight").highlight;
+const inquirer = require("inquirer");
+const nanospinner = require("nanospinner");
 
 const apiReq = async (text) => {
-  fetch("https://api.openai.com/v1/completions", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${currUser.apiKey}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      model: "text-davinci-003",
-      prompt: text,
-      max_token: 7,
-    }),
-  })
-    .then(async (ans) => {
-      const data = ans.json();
-      return data.choices[0].text;
-    })
-    .catch((err) => {
-      // in case of wrong api key
-      console.log(err.message());
-      return 0;
-    });
+  console.log(process.env.DEFAULT_API);
+  const configuration = new Configuration({
+    apiKey: process.env.DEFAULT_API,
+  });
+  const openai = new OpenAIApi(configuration);
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: `${text}`,
+      },
+    ],
+  });
+
+  const response = completion.data.choices[0].message.content;
+  if (
+    response.includes("code" || "CODE" || "Code") ||
+    text.includes("code" || "CODE" || "Code")
+  )
+    return highlight(response, { ignoreIllegals: true });
+  else return response;
 };
-module.exports = chat;
+
+function askQuestion(question) {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "answer",
+        message: question,
+      },
+    ])
+    .then((answers) => answers.answer);
+}
+
+function typeWithDelay(text, delay) {
+  return new Promise((resolve) => {
+    let currentIndex = 0;
+
+    function typeNextCharacter() {
+      process.stdout.write(text[currentIndex]);
+      currentIndex++;
+
+      if (currentIndex < text.length) {
+        setTimeout(typeNextCharacter, delay);
+      } else {
+        process.stdout.write("\n");
+        resolve();
+      }
+    }
+
+    typeNextCharacter();
+  });
+}
+
+async function main() {
+  while (true) {
+    const userInput = await askQuestion("Enter prompt: ");
+    if (userInput.toLowerCase().trim() === "exit") {
+      console.log("Goodbye...");
+      break; // Exit the while loop and end the program
+    }
+    var message;
+    try {
+      const messageSpinner = nanospinner
+        .createSpinner("Waiting for response...")
+        .start();
+
+      message = await apiReq(userInput); // Replace with your API call
+
+      messageSpinner.stop();
+    } catch (error) {
+      console.log(error);
+    }
+
+    const typingDelay = 20; // Delay between each character in milliseconds
+    await typeWithDelay(message, typingDelay);
+  }
+}
+
+// main();
+module.exports = main();
